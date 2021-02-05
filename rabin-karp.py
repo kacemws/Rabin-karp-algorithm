7f8291953d969ee6ce4cac7349bdb56db0011d5a
kacemws <berras.belkacem@gmail.com>
2021-02-05 13:43:06 +0100

    adding the function and the screenshot of the algorithm

---
+++ rabin-karp.py
@@ -0,0 +1,90 @@
+def prime_num(num) : 
+    if num > 1:
+        for i in range(2,num):
+            if (num % i) == 0:
+                return False
+        else:
+            return True
+    
+    else:
+        return False
+
+
+
+
+
+# d is the number of characters in the input alphabet (the radix to use)
+# q represents A prime number 
+
+def rabin_karp_matcher(text, pattern,d, q): 
+	m = len(pattern) ## length of the pattern we are looking for
+	n = len(text) ## length of the given text
+
+	p = 0 # hash value for pattern 
+	t = 0 # hash value for the window m of text 
+	h = 1
+
+	# The value of h would be "pow(d, m-1)%q" but it may take too long if m is large so here is how its computed manually
+	for i in range(m-1): 
+		h = (h*d)%q 
+
+	# preprocessing : Calculate the hash value of both the pattern and the first window of size m from the text
+	for i in range(m): 
+		p = (d*p + ord(pattern[i]))%q 
+		t = (d*t + ord(text[i]))%q 
+
+	# travel the text window by window, with each window being of size m, so there is no need to go to the last char, the (n-m)th char is our limit
+	for i in range(n-m+1): 
+		# if the hashed values match, then check the letters one by one
+		if p==t: 
+			for j in range(m): 
+				if text[i+j] != pattern[j]: 
+					break
+				else: j+=1
+
+			# if hashed values match and the window contains the same text as the pattern then print the found index
+			if j==m: 
+				print("Pattern Occurs with shift " + str(i)) 
+
+		# Calculate the hash value for the next window of text (under the condition that we didn't reach our limiter yet): Remove leading digit, add trailing digit 
+		if i < n-m: 
+			t = (d*(t-ord(text[i])*h) + ord(text[i+m]))%q 
+
+			# We might get negative values of t, converting it to 
+			# positive 
+			if t < 0: 
+				t = t+q
+
+
+def main() : 
+
+
+    showcase = True
+
+    while showcase : 
+        print('\nProvide text : ')
+        text = input() 
+        print('\nProvide pattern : ')
+        pattern = input() 
+        
+
+        prime = False
+        while not prime : 
+            print('\nProvide a prime number : ')
+            q = input()
+            prime = prime_num(int(q))
+
+        # q is a prime number of choice
+
+        d = 256
+
+        # Function Call 
+        rabin_karp_matcher(text,pattern,d, int(q)) 
+
+
+        print('\nContinue ? [Y/n]')
+        x = input()
+        showcase = x in ['y','Y']
+    
+
+main()
